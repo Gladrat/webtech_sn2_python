67a8809fcd14b7235c599b58a3c323dcb48b12ea
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-03-08 15:59:23 +0100

    update

--- django/gestion_entreprise/src/todo/urls.py
+++ django/gestion_entreprise/src/todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import tasks_list, task_details, task_list_created_by_date
+from .views import tasks_list, task_details, task_create, task_update, task_delete
 
 
 urlpatterns = [
-    path('', tasks_list, name="task-list"),
-    path('details/<int:id>/', task_details, name="task-list"),
+    path('list/', tasks_list, name="task-list"),
+    path('create/', task_create, name="task-create"),
+    path('details/<int:id>/', task_details, name="task-details"),
+    path('update/<int:id>/', task_update, name="task-update"),
+    path('delete/<int:id>/', task_delete, name="task-delete"),
 ]
--- django/gestion_entreprise/src/todo/views.py
+++ django/gestion_entreprise/src/todo/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def tasks_list(request):
-    return render(request=request, template_name="todo/tasks_list.dj.html")
+
+    return render(request=request, template_name="todo/list.dj.html", context = {})
 
 def task_details(request, id):
-    # return HttpResponse(f"<h1>Task details ({id}) :</h1>")
-    pass
+    context = {
+        "title": "Task details",
+        "id": id
+    }
+    return render(request=request, template_name="todo/details.dj.html", context=context)
+
+def task_create(request):
+    return render(request=request, template_name="todo/create.dj.html")
+
+def task_update(request, id):
+    return render(request=request, template_name="todo/update.dj.html")
+
+def task_delete(request, id):
+    return render(request=request, template_name="todo/delete.dj.html")
